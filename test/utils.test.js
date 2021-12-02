d7405130453dd7f2b53644cca3f98e0238287412
joshua360x <jkw5499@psu.edu>
2021-12-01 19:42:58 -0500

    Added more TDD test

--- test/utils.test.js
+++ test/utils.test.js
@@ -15,7 +15,31 @@ test('should return a string describing the current state', (expect) => {
     // Call the function you're testing and set the result to a const
     const actual = makeStatsString(4, 5, 1);
 
+
+    // Test 2 
+    const expected1 = `You have changed the Head State: 0 times 
+    Middle State: 0 times 
+    Bottom State: 0 times
+    And everybody loves your character's catchphrases:`;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual1 = makeStatsString(0, 0, 0);
+
+
+    // Test 3
+    const expected2 = `You have changed the Head State: 4 times 
+    Middle State: 5 times 
+    Bottom State: Negative Numbers are not possible
+    And everybody loves your character's catchphrases:`;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual2 = makeStatsString(4, 5, -1);
+
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+    expect.equal(actual1, expected1);
+    expect.equal(actual2, expected2);
 });
--- utils.js
+++ utils.js
@@ -12,13 +12,16 @@ export function makeStatsString(headCount, middleCount, bottomCount) {
     // if (middleCount === true) {
     //     middleCount++
     // }
-    // if (bottomCount === true) {
-    //     bottomCount++
+    // if (headCount < 0 || middleCount < 0 || bottomCount < 0) {
+        
+    //     return 'Negative Numbers are not possible'
+    // } else {
+        
     // }
+    return (`You have changed the Head State: ${headCount >= 0 ? `${headCount} times ` : 'Negative Numbers are not possible'}
+    Middle State: ${middleCount >= 0 ? `${middleCount} times ` : 'Negative Numbers are not possible'}
+    Bottom State: ${bottomCount >= 0 ? `${bottomCount} times` : 'Negative Numbers are not possible'}
+    And everybody loves your character's catchphrases:`);
 
 // console.log(headCount);
-    return (`You have changed the Head State: ${headCount} times 
-    Middle State: ${middleCount} times 
-    Bottom State: ${bottomCount} times
-    And everybody loves your character's catchphrases:`);
 }
